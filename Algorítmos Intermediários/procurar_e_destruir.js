5ed7b1593601839a0ef35e5379d538112881aced
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-11-16 20:29:06 -0300

    Adicionado script que remove todos os elementos passados como parametro da função de um array de numeros

---
+++ Algorítmos Intermediários/procurar_e_destruir.js
@@ -0,0 +1,26 @@
+// Entrada: Um array inicial e uma quantidade variável de
+// argumentos.
+
+// Saida: Um array como todos os elementos do array que são 
+// diferentes dos numeros passados como parametro.
+
+function destroyer(arr) {
+    let args = Array.from(arguments); // Resgata os argumentos passados para função
+                                      // como um array.
+    let newArr = [];
+    args.shift(); // Retira o primeiro elemento do array de argumentos, pois não
+                  // precisamos dele
+    for(let item of arr){
+      if(args.indexOf(item) === -1){
+        // Verfica todos os elementos que estão em 'arr' mas não são nenhum
+        // dos elementos passados como argumento da função.
+        newArr.push(item);
+      }
+    }
+    return newArr;
+    // O objeto 'arguments' é comum a todas as funções em javascript, nele
+    // contem todos os elementos passados como argumento da função.
+  }
+  
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+  
