c45a2f106140dda47a9baaaf9d3f0fe1edcac705
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-11-26 19:00:16 -0300

    Implementado o primeiro script para verificar se um determinado conjunto de caracteres é um palindromo

--- projetos-de-algoritmos-e-estruturas-de-dados-javascript/verificar_se_eh_palindromo.js
+++ projetos-de-algoritmos-e-estruturas-de-dados-javascript/verificar_se_eh_palindromo.js
@@ -17,3 +17,27 @@
     Nós também passaremos strings com símbolos especiais, como 
     2A3*3a2, 2A3 3a2 e 2_A3*3#A2.
 */
+
+
+function palindrome(str) {
+    /*
+        Coloca todos os caracteres da string em minusculo e 
+        substitui todas as não palavras e o underline da 
+        string por "".
+    */
+    str = str.toLowerCase().replace(/[\W_]/g, '');
+    for(var i = 0, len = str.length - 1; i < len/2; i++) {
+        /*
+            Faz uma checagem em todos os caracteres da string, sempre
+            espelhando a checagem, o caractere da str[i] deve ser igual
+            ao caractere str[len-i] para todo i, se não, a palavra não é
+            um palindromo.
+        */
+        if(str[i] !== str[len-i]) {
+            return false;
+        }
+    }
+    return true; /* Caso todas as verificações passem, retorna true
+                    ou seja, a string é um palindromo */
+  }
+
