ae25416fda3a34fde17b3343a220386b44bb4da8
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2022-11-29 22:13:14 -0300

    Adicionados casos de impossibilidade de retornar troco

--- projetos-de-algoritmos-e-estruturas-de-dados-javascript/caixa_registradora.js
+++ projetos-de-algoritmos-e-estruturas-de-dados-javascript/caixa_registradora.js
@@ -1,7 +1,6 @@
 function checkCashRegister(price, cash, cid) {
     let troco = cash - price;
     let escala = 100;
-    let caixa_registradora;
     let moedas = [
         ["PENNY",0.01],
         ["NICKEL",0.05],
@@ -13,16 +12,39 @@ function checkCashRegister(price, cash, cid) {
         ["TWENTY", 20],
         ["ONE HUNDRED", 100]
     ]
+    let total_caixa = 0;
+    for(let quantia of cid){ total_caixa += quantia[1]; } // calculo do total no caixa
+    total_caixa = total_caixa.toFixed(2); // Fixar duas casas decimais para o resultado
+
+    /* Checagens */
+    if(troco > total_caixa){ // Checa se há troco suficiente na caixa registradora
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+    else {
+        total_caixa = 0;
+        let caixa = moedas.filter(item => item[1] <= troco);
+        let troco_caixa = [];
+        for(let moeda of caixa){
+            troco_caixa.push(cid.filter(item => item[0] == moeda[0])[0]);
+            }
+        for(let moeda of troco_caixa){ total_caixa += moeda[1] }
+
+        if(total_caixa < troco){ // verificar se o troco pode ser dado nas moedas que tem em caixa
+            return {status: "INSUFFICIENT_FUNDS", change: []};
+        }
+    }
+    
+    
     /* 
         Utilizando filtro para fazer array para melhorar os calculos
     */
-    caixa_registradora = moedas.filter(item => item[1] <= troco);
-    for(let i = 0; i < caixa_registradora.length; i++){
-        let total_moeda_caixa = cid.filter(
-            item => item[0] === caixa_registradora[i][0]
-            )[0][1];
-            caixa_registradora[i].push(total_moeda_caixa)
-    }
+    // let caixa_registradora = moedas.filter(item => item[1] <= troco);
+    // for(let i = 0; i < caixa_registradora.length; i++){
+    //     let total_moeda_caixa = cid.filter(
+    //         item => item[0] === caixa_registradora[i][0]
+    //         )[0][1];
+    //         caixa_registradora[i].push(total_moeda_caixa)
+    // }
 
     /*
         O objeto caixa registradora deve estar no formato abaixo
@@ -31,15 +53,6 @@ function checkCashRegister(price, cash, cid) {
         ]
 
     */
-   let total = 0;
-   for(let moeda of caixa_registradora){ total += moeda[2]; }
-   
-   if(total < troco){ return {status: "INSUFFICIENT_FUNDS", change: []} }
-
-   if(total == troco) {
-       let obj  =  {status: "CLOSED", change: []}
-       
-    }
 }
 
   checkCashRegister(
